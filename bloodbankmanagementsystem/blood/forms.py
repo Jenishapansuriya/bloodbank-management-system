6461892dc0ba92eb4bb654a54643ebd9f2025346
Jenishapansuriya <pansuriyajenisha999@gmail.com>
2022-12-18 18:11:29 +0530

    bloodbank management system

---
+++ bloodbankmanagementsystem/blood/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from . import models
+
+
+class BloodForm(forms.ModelForm):
+    class Meta:
+        model=models.Stock
+        fields=['bloodgroup','unit']
+
+class RequestForm(forms.ModelForm):
+    class Meta:
+        model=models.BloodRequest
+        fields=['patient_name','patient_age','reason','bloodgroup','unit']
+
+class ContactusForm(forms.Form):
+    Name = forms.CharField(max_length=30)
+    Email = forms.EmailField()
+    Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
+
